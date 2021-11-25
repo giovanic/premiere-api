399358481a670680b340eb7e5fa5024596586d45
Giovani Carvalho <giovanic.miranda@gmail.com>
2021-11-25 11:07:30 -0300

    adicionado o metodo sign out

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { form: :json }, constraints: { subdomain: 'api'}, path: '/' do
     namespace :v1, path: '/' do
       resources :users, only: [:show, :create, :update, :destroy]
-      resources :sessions, only: [:create]
+      resources :sessions, only: [:create, :destroy]
     end
   end
 
